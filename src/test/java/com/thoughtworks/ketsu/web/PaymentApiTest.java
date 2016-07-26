11db2784d5d11fabf85bb75a37c26fbe0cb6588d
Ying Felix Zhou <ybzhou@thoughtworks.com>
2016-07-26 19:23:44 +0800

    return 400 when create payment with invalid params

--- src/test/java/com/thoughtworks/ketsu/web/PaymentApiTest.java
+++ src/test/java/com/thoughtworks/ketsu/web/PaymentApiTest.java
@@ -5,6 +5,7 @@ import com.thoughtworks.ketsu.domain.product.Product;
 import com.thoughtworks.ketsu.domain.product.ProductRepository;
 import com.thoughtworks.ketsu.domain.user.User;
 import com.thoughtworks.ketsu.domain.user.UserRepository;
+import com.thoughtworks.ketsu.infrastructure.mybatis.factory.ObjectFactory;
 import com.thoughtworks.ketsu.support.ApiSupport;
 import com.thoughtworks.ketsu.support.ApiTestRunner;
 import com.thoughtworks.ketsu.support.TestHelper;
@@ -16,6 +17,9 @@ import org.junit.runner.RunWith;
 import javax.inject.Inject;
 import javax.ws.rs.core.Response;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -46,10 +50,17 @@ public class PaymentApiTest extends ApiSupport{
     }
 
     @Test
-    public void should_return_201_when_create_payment() {
+    public void should_return_201_when_create_a_valid_payment() {
         final Response POST = post(paymentBaseUrl, TestHelper.paymentMap());
-        String test = POST.getLocation().toASCIIString();
         assertThat(POST.getStatus(), is(HttpStatus.CREATED_201.getStatusCode()));
         assertThat(Pattern.matches(".*?/users/[0-9-]*/orders/[0-9-]*/payment", POST.getLocation().toASCIIString()), is(true));
+    }
+
+    @Test
+    public void should_return_400_when_create_an_invalid_payment() {
+        final Response POST = post(paymentBaseUrl, new HashMap<String, Object>());
+        assertThat(POST.getStatus(), is(HttpStatus.BAD_REQUEST_400.getStatusCode()));
+        final List<Map<String, Object>> errorInfo = POST.readEntity(List.class);
+        assertThat(errorInfo.size(), is(2));
     }
 }
